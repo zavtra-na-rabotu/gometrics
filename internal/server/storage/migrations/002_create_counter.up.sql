f5bedaa57edb0ed6cf99aab6e8b41e3e07e86898
Vasilev Dmitry <mekon26@gmail.com>
2024-06-13 01:26:51 +0300

    INT -> BIGINT

--- internal/server/storage/migrations/002_create_counter.up.sql
+++ internal/server/storage/migrations/002_create_counter.up.sql
@@ -2,5 +2,5 @@ CREATE TABLE IF NOT EXISTS counter
 (
     id    SERIAL PRIMARY KEY,
     name  VARCHAR(255) UNIQUE NOT NULL,
-    value INT                 NOT NULL
+    value BIGINT              NOT NULL
 );
